c4f8c8e78e42ec3b06916f2b890efc1bdfad3ba2
Jason Peterson <jasonpet@us.ibm.com>
2019-11-08 12:18:25 -0500

    remove 403 workaround

--- provider/lib/utils.js
+++ provider/lib/utils.js
@@ -268,11 +268,7 @@ module.exports = function(logger, triggerDB, redisClient) {
                         }
                         logger.error(method, 'there was an error invoking', triggerData.id, statusCode || error);
 
-                        //temporary workaround for IAM SPI issue involving 403s
-                        if (statusCode && statusCode === HttpStatus.FORBIDDEN && triggerData.additionalData) {
-                            reject(`Received a 403 status code from IAM SPI: ${triggerData.id}`);
-                        }
-                        else if (statusCode && shouldDisableTrigger(statusCode)) {
+                        if (statusCode && shouldDisableTrigger(statusCode)) {
                             var message;
                             try {
                                 message = error.error.errorMessage;
