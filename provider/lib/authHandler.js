a4b7ffdfaedf32d75128a2404bccfe790109fb8d
Jason Peterson <jasonpet@us.ibm.com>
2021-02-21 22:10:08 -0500

    check if authString is defined when doing decryption

--- provider/lib/authHandler.js
+++ provider/lib/authHandler.js
@@ -58,33 +58,37 @@ function getToken(triggerData) {
 }
 
 function decryptAuth(authDBString) {
-    let authDBStringArray = authDBString.split('::');
-    if (authDBStringArray.length === 1) {
-        return authDBString;
-    } else if (authDBStringArray.length > 3) {
-        let key;
-        let cryptVersionID = authDBStringArray[2];
-        let base64NonceAndCiphertext = authDBStringArray[3];
-        if (cryptVersionID === encryptKeyID) {
-            key = encryptKeyValue;
-        } else if (cryptVersionID === encryptFallBackKeyID) {
-            key = encryptFallBackKeyValue
+    if (authDBString) {
+        let authDBStringArray = authDBString.split('::');
+        if (authDBStringArray.length === 1) {
+            return authDBString;
+        } else if (authDBStringArray.length > 3) {
+            let key;
+            let cryptVersionID = authDBStringArray[2];
+            let base64NonceAndCiphertext = authDBStringArray[3];
+            if (cryptVersionID === encryptKeyID) {
+                key = encryptKeyValue;
+            } else if (cryptVersionID === encryptFallBackKeyID) {
+                key = encryptFallBackKeyValue
+            } else {
+                return "";
+            }
+
+            // decode and retrieve nonce, ciphertext and tag
+            let nonceAndCiphertext = Buffer.from(base64NonceAndCiphertext, "base64");
+            let nonce = nonceAndCiphertext.slice(0, constants.ALGORITHM_NONCE_SIZE_12);
+            let ciphertext = nonceAndCiphertext.slice(constants.ALGORITHM_NONCE_SIZE_12, nonceAndCiphertext.length - constants.ALGORITHM_TAG_SIZE_16);
+            let tag = nonceAndCiphertext.slice(constants.ALGORITHM_NONCE_SIZE_12 + ciphertext.length);
+            // create cipher instance and set tag
+            let cipher = crypto.createDecipheriv(constants.ALGORITHM_AES_256_GCM, Buffer.from(key, "utf8"), nonce);
+            cipher.setAuthTag(tag);
+            // decrypt and return
+            return Buffer.concat([cipher.update(ciphertext), cipher.final()]).toString("utf8");
         } else {
             return "";
         }
-
-        // decode and retrieve nonce, ciphertext and tag
-        let nonceAndCiphertext = Buffer.from(base64NonceAndCiphertext, "base64");
-        let nonce = nonceAndCiphertext.slice(0, constants.ALGORITHM_NONCE_SIZE_12);
-        let ciphertext = nonceAndCiphertext.slice(constants.ALGORITHM_NONCE_SIZE_12, nonceAndCiphertext.length - constants.ALGORITHM_TAG_SIZE_16);
-        let tag = nonceAndCiphertext.slice(constants.ALGORITHM_NONCE_SIZE_12 + ciphertext.length);
-        // create cipher instance and set tag
-        let cipher = crypto.createDecipheriv(constants.ALGORITHM_AES_256_GCM, Buffer.from(key, "utf8"), nonce);
-        cipher.setAuthTag(tag);
-        // decrypt and return
-        return Buffer.concat([cipher.update(ciphertext), cipher.final()]).toString("utf8");
     } else {
-        return "";
+        return undefined;
     }
 }
 
